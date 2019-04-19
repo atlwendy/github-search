070b77b44c9efa91f9a969703af5fdd87e0ba4a9
Wendy Yang <atlwendy@gmail.com>
2019-04-19 14:33:41 -0400

    github search functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { map, startWith, switchMap, catchError, } from 'rxjs/operators';
+import { Observable, merge, of, forkJoin, } from 'rxjs';
+
+export interface GithubApi {
+  items: GithubUser[];
+  total_count: number;
+}
+
+export interface GithubUser {
+  [key: string]: any;
+}
+
+export class SearchHttpDao {
+  constructor(private http: HttpClient) { }
+
+  getUsers(term: string, page: number, perPage: number): Observable<GithubApi> {
+    const href = 'https://api.github.com/search/users';
+    const requestParams = `${href}?q=${term}&page=${page + 1}&per_page=${perPage}`;
+    return this.http.get<GithubApi>(`${requestParams}`);
+  }
+
+  getUser(data): Observable<GithubUser> {
+    return forkJoin(...data.items.map((d) => {
+      const href = `https://api.github.com/users/${d.login}`;
+      return this.http.get(`${href}`)
+        .pipe(
+          map((c: GithubUser) => {
+            return { 'followers': c.followers, 'name': c.name, 'bio': c.bio }
+          })
+        )
+    }));
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +43,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'github-search';
+  public searchForm: FormGroup = this.fb.group({});
+  delayApiResponse = false;
+  searchDatabase!: SearchHttpDao;
+  resultsLength = 0;
+
+  displayedColumns: string[] = ['name', 'login', 'bio', 'url', 'html_url', 'followers_count'];
+  dataSource = new MatTableDataSource<GithubUser>([]);
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+  ) { }
+
+  ngOnInit() {
+    this.searchForm = new FormGroup({
+      term: new FormControl('', []),
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  public search = (searchFormValue) => {
+    this.searchDatabase = new SearchHttpDao(this.http);
+    merge()
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          return this.searchDatabase
+            .getUsers(searchFormValue.term, this.paginator.pageIndex, this.paginator.pageSize)
+        }),
+        switchMap((data) => {
+          return this.searchDatabase.getUser(data)
+            .pipe(
+              map((newData) => {
+                data.items.concat(newData)
+                this.resultsLength = data.total_count;
+                let newItems = data.items;
+                newItems.map((item, index) => {
+                  item.followers_count = newData[index]['followers'];
+                  item.bio = newData[index]['bio'];
+                  item.name = newData[index]['name'];
+                  return item;
+                })
+                return data.items;
+              }));
+        }),
+        catchError(() => {
+          console.warn('GitHub API rate limit has been reached!');
+          return of([]);
+        }),
+      ).subscribe((data) => {
+        this.dataSource = new MatTableDataSource<GithubUser>(data);
+      });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule, MatPaginator, MatPaginatorModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
